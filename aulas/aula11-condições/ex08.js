feba2f91df5686504d2f583165863a2e463a5764
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-07-18 21:11:15 -0300

    condições part 1

---
+++ aulas/aula11-condições/ex08.js
@@ -0,0 +1,8 @@
+var vel = 79
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+
+if (vel > 60) { // Condição simples!
+    console.log('Você ultrapassou o limite permitido (60 Km/h). MULTADO!')
+}
+
+console.log('Dirija sempre com cinto de segurança!')
---
+++ aulas/aula11-condições/ex09.js
@@ -0,0 +1,10 @@
+var country = 'Brasil'
+
+console.log(`Vivendo em ${country}`)
+
+if (country != "Brasil") {
+    console.log('Você é Estrangeiro!')
+} else {
+    console.log('Você é Brasileiro!')
+}
+
