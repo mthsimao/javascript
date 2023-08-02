20e10ead2b539abe29e1492e1302c0cb58da892b
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-08-02 00:36:53 -0300

    começando em while

---
+++ aulas/aula13-repetições-while/ex016.js
@@ -0,0 +1,29 @@
+/*
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+
+Ao invés de eu fazer esse código gigante em cima, eu o faço utilizando
+a estrutura do while que é mais fácil e simples e claro, deixa o código
+mais limpo.
+*/
+
+/*
+
+var c = 1 
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ 
+} 
+
+*/
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
