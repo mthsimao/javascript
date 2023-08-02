7bb2bc6aad62c26bb348105509a8a88f3a05a4b1
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-08-02 00:38:44 -0300

    Update ex016.js

--- aulas/aula13-repetições-while/ex016.js
+++ aulas/aula13-repetições-while/ex016.js
@@ -7,23 +7,25 @@ console.log('Passo 5')
 console.log('Passo 6')
 
 Ao invés de eu fazer esse código gigante em cima, eu o faço utilizando
-a estrutura do while que é mais fácil e simples e claro, deixa o código
+a estrutura do while que é mais fácil e simples, e também deixa o código
 mais limpo.
+
 */
 
-/*
 
+// Primeiro testa, depois faz o bloco.
 var c = 1 
 while (c <= 6) {
     console.log(`Passo ${c}`)
     c++ 
 } 
 
-*/
+/* Primeiro faz o bloco, depois testa.
+
 var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
 } while (c <= 6)
 
-
+*/
