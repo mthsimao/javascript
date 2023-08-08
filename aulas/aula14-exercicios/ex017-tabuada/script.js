48f01da9913e8e607f567715e8d6a3633deadb9c
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-08-07 22:55:30 -0300

    exercício feito

--- aulas/aula14-exercicios/ex017-tabuada/script.js
+++ aulas/aula14-exercicios/ex017-tabuada/script.js
@@ -1,37 +1,20 @@
 function gerarTabuada() {
 
-    var checkInput = document.querySelector('input#inumber').value
-    var numeroInput = document.querySelector('input#inumber')
-    var res = document.querySelector('div#res')
+    let numeroInput = document.querySelector('input#inumber')
+    let tab = document.querySelector('select#iselect')
 
-    var number = Number(numeroInput.value)
-
-    if (checkInput === null || checkInput === "" ) {
+    if (numeroInput.value.length == 0 ) {
         window.alert('Por favor digite um número.')
     } else {
-        
+
+        let number = Number(numeroInput.value)
+        tab.innerHTML = ''
+
+        for (var x = 1; x <= 10 ; x++) {
+            let item = document.createElement('option')
+            item.text = `${number} x ${x} = ${number*x}`
+            item.value = `tab${x}`
+            tab.appendChild(item)
+        }
     }
-
-    // valores da tabuada
-    var x2 = number * 2
-    var x3 = number * 3
-    var x4 = number * 4
-    var x5 = number * 5
-    var x6 = number * 6
-    var x7 = number * 7
-    var x8 = number * 8
-    var x9 = number * 9
-    var x10 = number * 10
-
-    // resultado
-    res.innerHTML = `${number} x 1 = ${number} <br>`
-    res.innerHTML += `${number} x 2 = ${x2} <br>`
-    res.innerHTML += `${number} x 3 = ${x3} <br>`
-    res.innerHTML += `${number} x 4 = ${x4} <br>`
-    res.innerHTML += `${number} x 5 = ${x5} <br>`
-    res.innerHTML += `${number} x 6 = ${x6} <br>`
-    res.innerHTML += `${number} x 7 = ${x7} <br>`
-    res.innerHTML += `${number} x 8 = ${x8} <br>`
-    res.innerHTML += `${number} x 9 = ${x9} <br>`
-    res.innerHTML += `${number} x 10 = ${x10} <br>`
 }
