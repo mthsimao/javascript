151c69f44f34fd5b5b2bbd19f186bfc2898c6ec3
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-10-03 19:58:51 -0300

    arrow function

--- aulas/aula18-arrowFunctions/arrow.js
+++ aulas/aula18-arrowFunctions/arrow.js
@@ -2,4 +2,4 @@ const soma = (v1,v2) => {
     return v1 + v2
 }
 
-console.log(soma(10,5))
+console.log(soma(6,7))
--- aulas/aula18-arrowFunctions/node_1e7383c3b8b1e.tmp.js
+++
@@ -1,5 +0,0 @@
-const soma = (v1,v2) => {
-    return v1 + v2
-}
-
-console.log(soma(10,5))
