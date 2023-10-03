d82d2ddca04403456c02bc135efdfb48d8ce38c7
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-10-03 16:33:22 -0300

    Update arrow.js

--- aulas/aula18-arrow-function/arrow.js
+++ aulas/aula18-arrow-function/arrow.js
@@ -3,7 +3,15 @@ const soma = function(v1,v2) {
     return v1+v2
 }
 */
-
+/*
 const soma=(n1,n2)=>{return n1+n2}
 
 console.log(soma(2,5))
+
+*/
+
+
+let soma = (v1 , v2) => {
+    let vezes = soma(2,5)
+    console.log(vezes)
+}
