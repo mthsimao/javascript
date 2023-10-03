8016c1e3ab5890ff92820cf9e1e1ef329bc2d000
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-10-03 16:42:04 -0300

    Create node_05c4d89ad3b19.tmp.js

---
+++ aulas/aula18-arrow-function/node_05c4d89ad3b19.tmp.js
@@ -0,0 +1,19 @@
+/*
+const soma = function(v1,v2) {
+    return v1+v2
+}
+*/
+/*
+const soma=(n1,n2)=>{return n1+n2}
+
+console.log(soma(2,5))
+
+*/
+
+let multi = (n1, n2) => {
+    let multip = n1*n2
+    return multip
+}
+
+let multip = multi(2, 5)
+console.log(multip)
