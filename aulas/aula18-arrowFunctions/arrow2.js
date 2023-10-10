8e5ac6cb56478a468e893a4c5c342d744e29a70a
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-10-10 19:28:54 -0300

    Create arrow2.js

---
+++ aulas/aula18-arrowFunctions/arrow2.js
@@ -0,0 +1,13 @@
+function subtracao(a, b) {
+    return a - b
+}
+
+//Arrow function
+const subtracao2 = (a , b) => a - b
+
+console.log(subtracao2(7,2))
+
+
+// Caso a function receba apenas 1 parâmetro, não precisa de parênteses
+
+const somar2 = a => a + 2
