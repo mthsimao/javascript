77ca3ae903d67efbc943f654b41e11705f50de0f
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2023-12-26 21:32:46 -0300

    Update main.js

--- js-antes-do-react/main.js
+++ js-antes-do-react/main.js
@@ -8,37 +8,40 @@
     document.write('Sua idade é: ' + (idade ?? 'Não informado'))
 
 //---------------------------------
+*/
+
 
 // OBJETOS
 
 // Método in  
 
-    const user = {
+const user = {
 
-        name: 'Diego',
-        idade: 27,
-        address: {
-            rua: 'Rua valentina',
-            numeroRua: 214,
-        },
-    };
+    name: 'Diego',
+    idade: 27,
+    address: {
+        rua: 'Rua valentina',
+        numeroRua: 214,
+    },
+};
 
-    console.log( ('address' in user) )
+console.log( ('address' in user) )
 
-    // LEITURA DO MÉTODO in
-    // Existe 'address' dentro(in) de user ?
+// LEITURA DO MÉTODO in
+// Existe 'address' dentro(in) de user ?
+
 //---------------------------------------------
 
 // Método object 
 
-    // object.keys = retornar um vetor com todos os nomes de propriedades dentro de tal object
+// object.keys = retornar um vetor com todos os nomes de propriedades dentro de tal object
 
-    console.log( ( Object.keys(user) ) )
+console.log( ( Object.keys(user) ) )
 
-    // object.values = retornas todos os valores das propriedades dentro de tal object
+// object.values = retornas todos os valores das propriedades dentro de tal object
 
-    console.log( ( Object.values(user) ) )
+console.log( ( Object.values(user) ) )
 
 // JSON
 
-console.log
+console.log( JSON.stringify(user) )
