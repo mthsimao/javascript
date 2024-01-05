c6e4073df983765faab6220e878f95c29a31a4ce
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2024-01-05 16:11:06 -0300

    starting RegExp

---
+++ aulas/aula28-regex/regex.js
@@ -0,0 +1,11 @@
+let nome = "Bruno Pinho campos"
+
+console.log(nome);
+
+console.log(nome.search(/pinho/i));
+
+console.log(nome.match(/o/g));
+
+console.log(nome.replace(/pinho/i, 'Teste'));
+
+console.log(nome.replace(/o/ig, 'Teste'));
