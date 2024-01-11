65b4af5420cd6a4d6f5b0287df1f9e65dc053b29
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2024-01-10 22:05:26 -0300

    Update regex.js

--- aulas/aula28-regex/regex.js
+++ aulas/aula28-regex/regex.js
@@ -2,12 +2,17 @@ let nome = "Bruno Pinho campos"
 
 console.log(nome);
 
+// Procure a palavra "pinho" e me retorne a posição inicial
 console.log(nome.search(/pinho/i));
 
+// Encontre todas as letras "o"
 console.log(nome.match(/o/g));
 
+// Reponha a palavra "pinho" por "Teste"
 console.log(nome.replace(/pinho/i, 'Teste'));
 
+// Reponha todas as letras "o" por "Teste"
 console.log(nome.replace(/o/ig, 'Teste'));
 
+// encontre todos os "o" e "h" 
 console.log(nome.match(/[oh]/ig));
