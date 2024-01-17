a20a2d6be7a8d1422986c268325c83466620ff7e
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2024-01-17 15:36:11 -0300

    estrutura for

--- aulas/25.5-for/for.js
+++ aulas/25.5-for/for.js
@@ -1,19 +1,45 @@
-// Loops - definido ( for )
+// Loop - definido ( for )
 // utilizar o for quando eu souber a quantidade de vezes que eu preciso iterar o bloco de comando
 
-// estrutura for
+// estrutura for =>
+// (inicialização; condição de execução; incremento ou decremento)
 
-let array = [ 1, 2, 34, 3,5,6] 
-
-for (let i = 0; i<=array.length; i++) {
-    console.log(array[i]) 
+for(let i = 0; i<10; i++) {
+    console.log(`i = ${i}`)
 }
 
-// Loops -  indefinido (while and do while)
+// i=0, verifica se i < 10, se for true, i++ e executa o comando(s)
+// se a condição for false, ele para de executar o bloco de comando(s)
 
-// quando eu não souber a quantidade de vezes que preciso iterar o bloco de comando
-// eu utilizo o ( while ) ou ( do while )
+console.log('====================================');
+console.log('Brincadeira de ímpar ou par');
 
-// loop definido 
+// ip=0, verifica se ip < 20, se for, ip++
 
+// se ip%2 == 0, ou seja, se o valor de ip/2 for igual a 0, é par
 
+// mas se o valor de ip/2 for diferente de 0, é ímpar
+
+for(let ip = 0; ip < 20; ip++) {
+    if( ip%2==0) {
+        console.log(`${ip} é par`)
+    } else {
+        console.log(`${ip} é ímpar`);
+    }
+}
+
+console.log('====================================');
+
+console.log('For em array')
+
+let array = [1,2,3,4,5]
+
+// x=0, verifica se o x é menor ou igual o comprimento do array
+// se for menor, x++ => executa o comando
+
+for(let x = 0; x<=array.length; x++) {
+    console.log('Valor de x = ' + x)
+}
+
+//output > 1,2,3,4,5
+//-----------------------------------------------------------------
