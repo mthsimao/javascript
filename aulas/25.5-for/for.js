9903b2c29d26b43b3c3614720e7e796f913cac6d
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2024-01-17 15:04:03 -0300

    estrutura for

---
+++ aulas/25.5-for/for.js
@@ -0,0 +1,19 @@
+// Loops - definido ( for )
+// utilizar o for quando eu souber a quantidade de vezes que eu preciso iterar o bloco de comando
+
+// estrutura for
+
+let array = [ 1, 2, 34, 3,5,6] 
+
+for (let i = 0; i<=array.length; i++) {
+    console.log(array[i]) 
+}
+
+// Loops -  indefinido (while and do while)
+
+// quando eu não souber a quantidade de vezes que preciso iterar o bloco de comando
+// eu utilizo o ( while ) ou ( do while )
+
+// loop definido 
+
+
