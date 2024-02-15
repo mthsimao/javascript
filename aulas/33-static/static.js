21aa4b182ec9a83d0d376528fdc5d7390f53261e
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2024-02-15 14:57:42 -0300

    static in classes

--- aulas/33-static/static.js
+++ aulas/33-static/static.js
@@ -1,5 +1,6 @@
 class Npc {
     static alerta = false
+    // alerta default = false
     constructor(energia) {
         this.energia = energia
     }
@@ -7,15 +8,23 @@ class Npc {
     info = function(){
         console.log(`Energia: ${this.energia}`)
         console.log(`Alerta: ${Npc.alerta?"sim":"não"}`)
+        // não utilizo this.alerta pq está fora do constructor e é estática
         console.log('-------------------------')
+    }
+
+    static alertar = function() {
+        Npc.alerta = true
     }
 }
 
-const npc1 = new Npc(120)
+const npc1 = new Npc(100)
 const npc2 = new Npc(40)
 const npc3 = new Npc(80)
 
-// Npc.alerta = true
+// defini que todos os alertas estão true 
+// não preciso puxar todos os npc's e definir o alerta true
+
+Npc.alertar()
 
 npc1.info()
 npc2.info()
